6774c47f388fd22becba5b5bd9b55b37382981a5
Matieu <hei.tafita.2@gmail.com>
2025-08-10 06:30:52 -0400

    feat: integrate Vercel Analytics and Speed Insights for monitoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <SpeedInsights />
+      <Analytics />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
