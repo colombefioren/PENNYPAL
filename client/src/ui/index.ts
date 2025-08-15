e49ad42a2c426d16241b7e2d768b0f20f55c1453
Matieu <hei.tafita.2@gmail.com>
2025-08-15 14:15:37 -0400

    chore: update dependencies in package.json

---
+++ client/src/ui/index.ts
@@ -0,0 +1,12 @@
+export { default as Button } from './Button';
+export { default as Tooltip } from './Tooltip';
+export { default as Chip } from './Chip';
+export { default as Toast, ToastProvider, ToastContainer, useToast } from './Toast';
+export { default as IconButton } from './IconButton';
+
+
+export type { ButtonProps } from './Button';
+export type { TooltipProps } from './Tooltip';
+export type { ToastProps, ToastContextType } from './Toast';
+export type { ChipProps } from './Chip';
+export type { IconButtonProps } from './IconButton';
