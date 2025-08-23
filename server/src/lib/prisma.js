387ba2ad07edb59134c950e97a086c968ff2e946
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-23 13:21:17 +0300

    refactor: change prisma.ts to prisma.js

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,8 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = globalThis;
+globalForPrisma.prisma = globalForPrisma.prisma || undefined;
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
