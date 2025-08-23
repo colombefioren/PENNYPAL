867f846c413cbf16cf687ed76df975eef47a1e0c
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-23 20:59:45 +0300

    feat: income routes

---
+++ server/routes/income.route.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { createIncome, getIncomes, getIncome, updateIncome, deleteIncome, getIncomeCategories, createIncomeCategory, updateIncomeCategory, deleteIncomeCategory } from '../controllers/incomeController';
+
+const router = Router();
+
+//INCOME
+router.get('/', getIncomes);
+router.get('/:id', getIncome);
+router.post('/', createIncome);
+router.put('/:id', updateIncome);
+router.delete('/:id', deleteIncome);
+
+//INCOME CATEGORY
+router.get('/income-categories/list', getIncomeCategories);
+router.post('/income-categories', createIncomeCategory);
+router.put('/income-categories/:id', updateIncomeCategory);
+router.delete('/income-categories/:id', deleteIncomeCategory);
+
+export default router;
