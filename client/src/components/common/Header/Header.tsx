4af78112df326d83526b067c7b5953b0172ec083
colombefioren <colomberakotonjanahary@gmail.com>
2025-08-25 12:44:59 +0300

    style: remove profile pic square border

--- client/src/components/common/Header/Header.tsx
+++ client/src/components/common/Header/Header.tsx
@@ -10,9 +10,7 @@ const DashboardHeader = () => {
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold text-white">
           Hi,{" "}
-          <span className="bg-accent bg-clip-text text-transparent">
-            John
-          </span>
+          <span className="bg-accent bg-clip-text text-transparent">John</span>
         </h1>
         <p className="text-indigo-100/90 font-light">
           Track all your transactions with PennyPal
@@ -33,13 +31,12 @@ const DashboardHeader = () => {
 
           <Link to={"/profile"} className="flex items-center gap-3 group">
             <div className="relative">
-              <div className="absolute inset-0 bg-gradient-to-br from-amber-400 to-amber-600 rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
               <img
                 src={assets.userPlaceholder}
-                className="w-12 h-12 object-cover rounded-xl border-2 border-white/30 relative z-10 group-hover:border-amber-200/50 transition-all duration-300"
+                className="w-13 h-13 object-cover relative z-10 group-hover:border-amber-200/50 transition-all duration-300"
                 alt="Profile"
               />
-              <div className="absolute bottom-0 -right-1 w-3.5 h-3.5 bg-green-400 rounded-full border-2 border-white z-20"></div>
+              <div className="absolute bottom-0 right-0 w-3.5 h-3.5 bg-green-400 rounded-full border-2 border-white z-20"></div>
             </div>
 
             <div className="flex flex-col">
@@ -47,7 +44,7 @@ const DashboardHeader = () => {
                 John Doe
               </span>
               <span className="text-xs text-white/70 group-hover:text-amber-100/80 transition-colors duration-300">
-                Premium Client
+                Client
               </span>
             </div>
 
